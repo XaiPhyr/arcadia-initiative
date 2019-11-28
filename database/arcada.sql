e564ce0ab5ec4de065e0a28d3f3c9498b5175844
xaiphyr <arcadia.initiative@gmail.com>
2019-11-28 20:51:05 +0800

    added tables

--- database/arcada.sql
+++ database/arcada.sql
@@ -19,3 +19,25 @@
 --     `xguid` varchar(10)
 -- )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+ALTER TABLE arcadia.users AUTO_INCREMENT=10001;
+
+CREATE TABLE arcadia.users_header(
+    `Id` int(5) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `xguid` int(5),
+    `first_name` varchar(50),
+    `last_name` varchar(50),
+    `suffix` varchar(15)
+)ENGINE=InnoDB AUTO_INCREMENT=20001 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE arcadia.users_detail(
+    `xguid` int(5),
+    `area_code` varchar(50),
+    `contact_number` varchar(50),
+    `address` varchar(50),
+    `zone` varchar(50),
+    `region` varchar(50),
+    `city` varchar(50),
+    `country` varchar(50),
+    `postal_code` varchar(50)
+)ENGINE=InnoDB AUTO_INCREMENT=20001 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
